3d080ea973a515a34e7c4b20ad1d5c2a94ca8f7a
Rishabh Varshney <right.rishabh@gmail.com>
2024-08-21 10:15:31 +0530

    chore: Refactor main.dart for improved code readability and performance

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:rishabh_profile/home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    initialRoute: '/',
-    debugShowCheckedModeBanner: false,
-    routes: {
-      '/': (context) => const HomePage(),
-    },
-  ));
+  runApp(const MaterialApp(home: HomePage()));
 }
